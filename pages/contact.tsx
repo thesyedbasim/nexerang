a0b45792b3672f7f833b4451250c2ddfb2e36772
Syed Basim <social.syedbasim@gmail.com>
2023-02-02 15:49:13 +0530

    edit copy

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -10,9 +10,9 @@ import { SectionCopyHeader } from '../types/CopyTypes';
 
 const Contact = () => {
 	const SEO = useRef({
-		title: 'Contact a Website & UI/UX Designer Freelancer | Syed Basim',
+		title: 'Contact Website design and development agency | Nexerang',
 		description:
-			'If you want grow your online presence with a great and reliable webiste, you can contact me and I would help you increase your sales and your business.',
+			'If you want grow your online presence with a great and reliable webiste, you can contact Nexerang and we would help you increase your sales and your business.',
 		url: 'https://www.nexerang.com/contact',
 		canonical: 'https://www.nexerang.com/contact',
 		openGraph: {
@@ -26,10 +26,10 @@ const Contact = () => {
 
 	const contactSection: MutableRefObject<ContactTextProps> = useRef({
 		title: 'Contact',
-		body: "If you're interested to do business with me and would like to grow your online presence, you can fill up the form and I will be ready to carefully arrange the quote according to your needs.",
+		body: "If you're interested to do business with us and would like to grow your online presence, you can fill up the form and we will be ready to carefully arrange the quote according to your needs.",
 		contact: [
 			{
-				text: 'business@syedbasim.com'
+				text: 'business@nexerang.com'
 			}
 		]
 	});
